b05ccad6078fa36125d92b74c3f317a2900af950
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-07-23 10:00:15 +0530

    added code to display the image

--- Online Evaluation System/src/com/winpoint/oes/dao/LoginDao.java
+++ Online Evaluation System/src/com/winpoint/oes/dao/LoginDao.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.swing.ImageIcon;
+
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.winpoint.oes.beans.UserProfile;
 
 public class LoginDao {
 	UserProfile userProfile = null;
-	public UserProfile login(String email, String password) {
+	public UserProfile login(String emailId, String password) {
 		boolean flag = false;
 		ResultSet resultSet = null;
 		SQLServerDataSource dataSource = new SQLServerDataSource();
@@ -23,38 +25,37 @@ public class LoginDao {
 		try(Connection connection = dataSource.getConnection()){
 			Statement statement = connection.createStatement();
 			
-			String query = "SELECT	USER_PROFILE.USERID,\r\n" + 
+			String query = "SELECT	USER_PROFILE.USER_ID,\r\n" + 
 					"		FIRST_NAME, \r\n" + 
-					"		EMAIL, \r\n" + 
-					"		PHOTO, \r\n" + 
-					"		USER_PROFILE.USER_CATEGORY_ID, \r\n" + 
-					"		USER_CATEGORY.USER_CATEGORY_NAME ,\r\n" + 
-					"		EMPLOYEE_CATEGORY.EMPLOYEE_CATEGORY_NAME\r\n" + 
-					"	FROM USER_PROFILE  \r\n" + 
-					"		JOIN USER_CATEGORY \r\n" + 
+					"		LAST_NAME, \r\n" + 
+					"		PHOTO_LOCATION, \r\n" + 
+					"		USER_PROFILE.USER_CATEGORY_ID,\r\n" + 
+					"		EMPLOYEE_CATEGORY.EMPLOYEE_CATEGORY_ID\r\n" + 
+					"	FROM USER_PROFILE JOIN USER_CATEGORY \r\n" + 
 					"		ON USER_PROFILE.USER_CATEGORY_ID = USER_CATEGORY.USER_CATEGORY_ID\r\n" + 
 					"			LEFT OUTER JOIN EMPLOYEE_DETAILS D\r\n" + 
-					"			ON USER_PROFILE.USERID = D.USERID\r\n" + 
+					"			ON USER_PROFILE.USER_ID = D.USER_ID\r\n" + 
 					"				LEFT OUTER JOIN EMPLOYEE_CATEGORY\r\n" + 
 					"				ON D.EMPLOYEE_CATEGORY_ID = EMPLOYEE_CATEGORY.EMPLOYEE_CATEGORY_ID\r\n" + 
-					"	WHERE (USER_PROFILE.EMAIL = '" + email + "') AND USER_PROFILE.PASSWORD = '" + password + "'";
-			
+					"	WHERE USER_PROFILE.EMAIL_ID = '" + emailId + "' AND USER_PROFILE.PASSWORD = '" + password + "'";
 			resultSet = statement.executeQuery(query);
 			while(resultSet.next()) {
-				int userId = resultSet.getInt("userid");
+				int userId = resultSet.getInt("user_id");
 				String firstName = resultSet.getString("first_name");
+				String lastName = resultSet.getString("last_name");
 				//String email = resultSet.getString("email");
 				int userCategoryId = resultSet.getInt("USER_CATEGORY_ID");
-				String userCategoryName = resultSet.getString("USER_CATEGORY_NAME");
-				String employeeCategoryName = resultSet.getString("EMPLOYEE_CATEGORY_NAME");
-				//String photo = resultSet.getString("PHOTO");
+				//String userCategoryName = resultSet.getString("USER_CATEGORY_NAME");
+				String employeeCategoryId = resultSet.getString("EMPLOYEE_CATEGORY_ID");
+				String photoLocation = resultSet.getString("PHOTO_LOCATION");
+				//String photoLocation = "/OnlineEvaluationSystem/img/photos/client/Knitting Balls.jpg";
 	            System.out.println(userId);
 	            System.out.println(firstName);
 	            System.out.println(userCategoryId);
-	            System.out.println(userCategoryName);
-	            System.out.println(employeeCategoryName);
+	            //System.out.println(userCategoryName);
+	            System.out.println(employeeCategoryId);
 	            
-	            userProfile = new UserProfile(userId, firstName, email, userCategoryId);
+	            userProfile = new UserProfile(userId, firstName, lastName, emailId, userCategoryId, photoLocation);
 				flag = true;
 			}
 		}
