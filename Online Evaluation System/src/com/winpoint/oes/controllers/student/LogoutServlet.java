6d4dbd76cd28c9e1014ccafd1bee67d6b4fadd42
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-12-31 14:46:04 +0530

    added logout functionality

---
+++ Online Evaluation System/src/com/winpoint/oes/controllers/student/LogoutServlet.java
@@ -0,0 +1,57 @@
+package com.winpoint.oes.controllers.student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("In LogoutServlet");
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		Gson gson = new Gson();
+		String json1 = null;
+		String jsonString = null;
+		json1 = gson.toJson("{ 'success': 'true', 'location': '/OnlineEvaluationSystem/jsp/LoginForm.jsp'}");
+		jsonString = "[" + json1 + "]";
+		
+		PrintWriter writer = response.getWriter();
+		writer.println(jsonString);
+		writer.flush();
+	}
+
+}
