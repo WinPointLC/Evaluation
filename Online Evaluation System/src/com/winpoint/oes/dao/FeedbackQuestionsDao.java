b9f4fddb10fb1393b79b8ee337cc71e1e17483b7
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2020-01-03 17:50:11 +0530

    updated  feedbackform functionality

--- Online Evaluation System/src/com/winpoint/oes/dao/FeedbackQuestionsDao.java
+++ Online Evaluation System/src/com/winpoint/oes/dao/FeedbackQuestionsDao.java
@@ -1,10 +1,13 @@
 package com.winpoint.oes.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
